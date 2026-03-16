65bae8ed18d0c57939858efdd9ccb272459ac07b
Alex Paquibot <115098095+alexpqbt@users.noreply.github.com>
2026-03-16 17:35:49 +0800

    feat: implement basic stuff
    but the speech-to-text is shit because i am using a free one
    plus with my shitty mic. i can't do it lmao. ill deal with this
    later when i researched more about this one.

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from daddyshome!")
+import speech_recognition as sr
+import time
 
+recognizer = sr.Recognizer()
+
+def callback(recognizer, audio):
+    try:
+        text = recognizer.recognize_sphinx(audio)
+        print(text)
+    except sr.UnknownValueError:
+        print("No understand")
+
+def main():
+    with sr.Microphone() as source:
+        recognizer.adjust_for_ambient_noise(source, duration=2)
+
+    stop_listening = recognizer.listen_in_background(source, callback)
+
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        stop_listening()
 
 if __name__ == "__main__":
     main()
